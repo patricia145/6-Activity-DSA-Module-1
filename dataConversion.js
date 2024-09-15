66d0a68c5f6fe2fdc1924b92e7be5b61ea9c4c3e
patricia145 <patriciavaldez0517@gmail.com>
2024-09-15 20:03:54 +0800

    initial commit

---
+++ dataConversion.js
@@ -0,0 +1,20 @@
+let price = "200";
+
+let p = parseInt(price);
+console.log("Using parseInt():", p);
+let a = Number(price);
+console.log("Using Number():", a);
+let t = +price;
+console.log("Using + operator:", t);
+
+
+let distance = 150;
+let x = distance.toString();
+console.log("Using toString():", x);
+let j = String(distance);
+console.log("Using String():", j);
+
+
+let weight = "72.5";
+let b = parseFloat(weight);
+console.log("Using parseFloat():", b);
